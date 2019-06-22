5ec117b14e72d1f005b5c39fbc96ba3887759c62
Tal Monso <monsotal@gmail.com>
2019-06-22 21:02:11 +0300

    1st commit to local repo

---
+++ src/com/debian_config/Main.java
@@ -0,0 +1,15 @@
+package com.debian_config;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		System.out.println("Welcome To Debian Config tool");
+
+	}
+
+}
