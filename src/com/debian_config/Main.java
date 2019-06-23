596a83abb4646987a18be5fff325482b51e3a1e8
Tal Monso <monsotal@gmail.com>
2019-06-23 22:32:04 +0300

    initial menu added..

--- src/com/debian_config/Main.java
+++ src/com/debian_config/Main.java
@@ -1,5 +1,7 @@
 package com.debian_config;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -7,8 +9,37 @@ public class Main {
 		
 		
 		
-		
 		System.out.println("Welcome To Debian Config tool!");
+		
+//This reads the input provided by user
+//using keyboard
+		
+		Scanner userinput = new Scanner(System.in);
+
+
+	    // Display menu graphics
+	    System.out.println("============================");
+	    System.out.println("|   MENU SELECTION DEMO    |");
+	    System.out.println("============================");
+	    System.out.println("| Options:                 |");
+	    System.out.println("|        1. Option 1       |");
+	    System.out.println("|        2. Option 2       |");
+	    System.out.println("|        3. Exit           |");
+	    System.out.println("============================");
+	    
+	    
+// This method reads the number provided using keyboard
+	    
+	    System.out.println("Pick your option:");
+        int num = userinput.nextInt();
+        
+// Closing Scanner after the use   
+        
+        userinput.close();
+        
+        System.out.println("You picked:"+num);
+	    
+
 
 	}
 
