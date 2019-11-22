368338306d76ea6339f015e26f6c468f1e7ca92c
Tal Monso <monsotal@gmail.com>
2019-11-22 14:45:57 +0200

    added changes to permitRootLogin method

--- src/com/debian_config/Main/Main.java
+++ src/com/debian_config/Main/Main.java
@@ -286,7 +286,7 @@ public class Main {
 			
 
 			 ProcessBuilder pb = new ProcessBuilder();
-			 pb.command("bash", "-c", "touch newfile");
+			 pb.command("bash", "-c", "service ssh restart");
 			 pb.start();
 
 			System.out.println("SSH login with 'root' user is now permitted");
